456010dcb54494412bc3c2b8d0497ef44ed19a5f
kendallgoodwin <kendall@kendallgoodwin.com>
2016-05-23 15:34:39 -0700

    fruits and veggies in wells in center column with clickable arrows

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,6 +1,29 @@
 var app = angular.module("FVSorterApp", []);
 app.controller("HomeCtrl", ['$scope', function($scope, $http) {
-	
+
+	$scope.primary = true;
+	$scope.small = true;
+
+	$scope.left = [];
+	$scope.right = [];
+
+	$scope.fruitVeggies = [
+			'Banana',
+			'Apple',
+			'Apricot',
+			'Asparagus',
+			'Artichoke',
+			'Arugula'
+	];
+
+	$scope.toCol1 = function() {
+		$scope.left.push()
+	}
+
+	$scope.toCol2 = function() {
+		$scope.right.push($scope.li)
+	}
+
 }]);
 
 //debug stuff to show the app is loading and fruit / veggies are available
